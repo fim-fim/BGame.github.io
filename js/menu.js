ab6fb6e2416f97ba63deea05c2cecf6f1cbc64a0
fim-fim <fim2.11.11.11@gmail.com>
2024-07-27 14:03:30 +0900

    last updata

--- js/menu.js
+++ js/menu.js
@@ -1,3 +1,12 @@
 document.getElementById('menuOpen').addEventListener('click',()=>{
     document.getElementById('menuScreenBack').style.display = 'block';
 })
+document.getElementById('menuClose').addEventListener('click',()=>{
+    document.getElementById('menuScreenBack').style.display = 'none';
+})
+document.getElementById('adminIn').addEventListener('click',()=>{
+    document.getElementById('admin').style.display = 'block';
+})
+document.getElementById('adminOut').addEventListener('click',()=>{
+    document.getElementById('admin').style.display = 'none';
+})
